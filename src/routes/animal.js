2bd218b9989af2c4f3de856e04f78338de94676e
zavithar <mondongo@gmail.com>
2025-10-23 20:30:17 -0500

    v1.5

---
+++ src/routes/animal.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const animalSchema = require("../models/animalModel");
+const verifyToken = require('./validate_token');
+
+//Nuevo animal
+router.post("/animalitos", (req, res) => {
+    const animal = animalSchema(req.body);
+    animal
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/animals", verifyToken,(req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+//Consultar un animal por su idzzzzzzzzzzzz<
+router.get("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.put("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, edad, tipo, fecha } = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, edad, tipo, fecha }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+router.delete("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
+
+router.get("/animalsgt", (req, res) => {
+    
+    animalSchema
+        .find({edad:{$gt:0}})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
+
+
+
